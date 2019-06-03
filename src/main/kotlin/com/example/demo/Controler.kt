38ec70a6ea4d6684d05112c57db768cb97843e04
Ricardo Rocha <ricardo.rocha@talkdesk.com>
2019-06-03 17:18:20 +0100

    Add new HTTP POST id
    added new function to receive id inside the path
    
    RP-357

--- src/main/kotlin/com/example/demo/Controler.kt
+++ src/main/kotlin/com/example/demo/Controler.kt
@@ -1,12 +1,8 @@
 package com.example.demo
 
-import com.example.demo.model.BasicUser
 import com.example.demo.model.idWrapper
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class Controler(private val repo: com.example.demo.database.Repository){
@@ -16,27 +12,18 @@ class Controler(private val repo: com.example.demo.database.Repository){
         return ResponseEntity.ok().body(repo.findAll())
     }
 
-
-    @PostMapping("/")
-    fun addUser(@RequestBody user: BasicUser ) : ResponseEntity<Any> {
-        val s = repo.save(user)
-        println("POST of user with id ${s.id}")
-        return ResponseEntity.ok().body(s);
-    }
-
     @PostMapping("/ID/")
     fun logID(@RequestBody id: idWrapper ) : ResponseEntity<Any> {
         println("POST of user with id ${id.id}")
         return ResponseEntity.ok().body(id);
     }
 
-    /**
-     *
-    @PostMapping("/")
-    fun addUser(@RequestBody id: Int) : ResponseEntity<Any> {
-    println("Received id $id")
-    return ResponseEntity.ok().build();
+    @PostMapping("/ID/{id}")
+    fun logID2(@PathVariable id: Int) : ResponseEntity<Any> {
+        println("POST of user with id $id")
+        return ResponseEntity.ok().body(id);
     }
-     */
+
+
 
 }
