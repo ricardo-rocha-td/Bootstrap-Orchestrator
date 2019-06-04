29722cbc61a7293764d3694ac749d788cb99aa24
Ricardo Rocha <ricardo.rocha@talkdesk.com>
2019-06-04 14:03:29 +0100

    Add thread pool for running configs
    Finished the second part of the Step 2.2 and added a thread pool that runs configurations

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -4,6 +4,7 @@ import com.example.demo.model.Configuration
 import com.example.demo.model.Step
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.concurrent.Executors
 
 @SpringBootApplication
 class DemoApplication
@@ -16,13 +17,14 @@ fun main(args: Array<String>) {
 	ConfigExec()
 }
 
-class ConfigExec(){
+class ConfigExec{
 
-	val list = mutableListOf<Configuration>()
+	private val list = mutableListOf<Configuration>()
 
 	init {
 		loadConfig()
-		runConfigs()
+		//runConfigs()
+		runConfigsWithThread()
 	}
 
 	/**
@@ -30,7 +32,7 @@ class ConfigExec(){
 	 *
 	 */
 
-	fun loadConfig(){
+	private fun loadConfig(){
 		println("Loading hard coded configs \n")
 
 		var steps = mutableMapOf<Int,Step>()
@@ -45,35 +47,57 @@ class ConfigExec(){
 		list.add(Configuration(2,1,steps))
 
 		list.forEach{ c:Configuration -> println(c)}
+
 	}
 
-	fun runConfigs(){
+	private fun runConfigs(){
 		println("\n \n[RUNNING CONFIGS] \n ")
 
-		list.forEach {
-			c : Configuration ->
-
-			println("Running Config: ${c.id} \nInitial Step: ${c.initialStep}")
-			nextStep(c.initialStep,c.steps)
-		}
+		list.forEach { c : Configuration -> firstStep(c) }
 	}
 
-	fun nextStep(step : Int, map: Map<Int,Step>){
+	private fun firstStep(c : Configuration){
+		println("Running Config: ${c.id} \nInitial Step: ${c.initialStep}")
+		nextStep(c.initialStep,c.steps)
+	}
+
+	private fun nextStep(step : Int, map: Map<Int,Step>){
 		val s = map.get(step)
 		if (s != null){
-			var log = "Running step: $step, Action: ${s.name}"
+			val log = "Running step: $step, Action: ${s.name}"
 
 			if (s.transaction == null) {
 				println(log)
 				println("No more steps \n")
 			}
 			else {
-				println(log + ", Next: ${s?.transaction}")
+				println(log + ", Next: ${s.transaction}")
 				nextStep(s.transaction,map)
 			}
+		}
+	}
 
+	/**
+	 *  STEP 2 - with thread pool
+	 *
+	 */
+
+	private fun runConfigsWithThread(){
+		println("\n \n[RUNNING CONFIGS] \n ")
+
+		val executor = Executors.newFixedThreadPool(5)
+		for (c in list) {
+			val worker = Runnable {
+				println("Thread " + Thread.currentThread().id )
+				firstStep(c)
+			}
+			executor.execute(worker)
 		}
 
+		executor.shutdown()
+		while (!executor.isTerminated) {
+		}
+		println("Finished all threads")
 	}
 
 }
