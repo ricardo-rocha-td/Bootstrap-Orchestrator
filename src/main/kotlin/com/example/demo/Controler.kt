91645b076bfcfc180631468e040efd6f233ecde8
Ricardo Rocha <ricardo.rocha@talkdesk.com>
2019-06-04 14:45:00 +0100

    Change removed mongoDB for easy heroku set up

--- src/main/kotlin/com/example/demo/Controler.kt
+++ src/main/kotlin/com/example/demo/Controler.kt
@@ -2,17 +2,21 @@ package com.example.demo
 
 import com.example.demo.model.idWrapper
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class Controler(private val repo: com.example.demo.database.Repository){
+class Controler(){
+    //private val repo: com.example.demo.database.Repository
 
 
     //get objects on repo
-    @GetMapping("/")
-    fun getAll() : ResponseEntity<Any> {
-        return ResponseEntity.ok().body(repo.findAll())
-    }
+    //@GetMapping("/")
+    //fun getAll() : ResponseEntity<Any> {
+    //    return ResponseEntity.ok().body(repo.findAll())
+    //}
 
     //as json
     @PostMapping("/ID/")
